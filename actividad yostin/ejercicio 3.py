9b17c2435ad5bbf76110fd2a60af790b3ae1f228
Rostin28 <yostinruiz.28@gmail.com>
2019-02-21 21:42:40 -0600

    actividad de 30 ejercicios 21/02/19

--- actividad yostin/ejercicio 3.py
+++ actividad yostin/ejercicio 3.py
@@ -9,5 +9,8 @@ if hr > 0 and hr < 5:
     print ("tu precio a pagar es:.{}".format(total))
 elif hr >= 5:
     total = BS * hr
-    print ("tienes una hora mas gratis:.")
     print ("tienes que pagar:.{}".format(total))
+    if hr >=5:
+        total2 = hr / 5
+        print("LAS HORAS GRATIS Q TIENES SON")
+        print(int(total2))
